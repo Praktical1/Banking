7367f6904b64e051011d62c5c9340b639ca026ce
Prakhash Rajkumar <prakhash.rajkumar@yahoo.co.uk>
2022-05-18 16:41:30 +0100

    trying to update before continuing

--- src/Current.java
+++ src/Current.java
@@ -1,5 +1,23 @@
 public class Current {
+    private int BankNumber;
+    private int SortCode;
+    private int Balance;
     public void transfer(int BankNumber, int SortCode, int RecipientBankNumber, int RecipientSortCode, int Transferred) {
+        if (Balance<Transferred) {
+            System.out.println("Insufficient Balance");
+        } else {
+            Withdraw(BankNumber,SortCode,Transferred);
+            if (AccountType.Equals("ISA"){
+                if ISA.
+            }
+            Deposit(RecipientBankNumber,RecipientSortCode,Transferred);
+        }
+    }
+    private void Deposit(int BankNumber, int SortCode, int Deposited) {
+        Balance = Balance + Deposited;
+    }
 
+    private boolean Withdraw(int bankNumber, int sortCode, int Withdrawn) {
+        Balance = Balance - Withdrawn;
     }
 }
