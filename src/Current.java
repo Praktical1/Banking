1f773990f0f6ba01df36f0c6a7a55419f5160907
Prakhash Rajkumar <prakhash.rajkumar@yahoo.co.uk>
2022-05-18 17:04:25 +0100

    Attempt to do current

--- src/Current.java
+++ src/Current.java
@@ -3,18 +3,29 @@ public class Current {
     private int SortCode;
     private int Balance;
     public void transfer(int BankNumber, int SortCode, int RecipientBankNumber, int RecipientSortCode, int Transferred) {
+        Balance = Bank_Accounts.getBalance();
+        String AccountType = Bank_Accounts.getAccountType();
         if (Balance<Transferred) {
             System.out.println("Insufficient Balance");
         } else {
             Withdraw(BankNumber,SortCode,Transferred);
-            if (AccountType.Equals("ISA"){
-                if ISA.
+            if (AccountType.equals("ISA")) {
+                if (ISA.AnnualDeposit + Transferred < ISA.Max_Annual_Deposit) {
+                    Deposit(RecipientBankNumber,RecipientSortCode,Transferred);
+                    Withdraw(BankNumber,SortCode,Transferred);
+                } else {
+                    System.out.println("Cannot transfer, Annual deposit limit will be reached");
+                }
+            } else if (AccountType.equals("Business")) {
+                Deposit(RecipientBankNumber,RecipientSortCode,Transferred);
+                Withdraw(BankNumber,SortCode,Transferred);
+            } else {
+                System.out.println("Unrecognized account type, check code");
             }
-            Deposit(RecipientBankNumber,RecipientSortCode,Transferred);
         }
     }
     private void Deposit(int BankNumber, int SortCode, int Deposited) {
-        Balance = Balance + Deposited;
+        Bank_Accounts.setBalance() = Bank_Accounts.getBalance() + Deposited;
     }
 
     private boolean Withdraw(int bankNumber, int sortCode, int Withdrawn) {
