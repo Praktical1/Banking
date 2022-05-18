7949f78aca4c6df9b429d26cc04e0f373f8d7aec
Roy Abrahamson <abrahamsonroy@gmail.com>
2022-05-18 15:11:52 +0100

    Created class constructors

--- src/Bank_Accounts.java
+++ src/Bank_Accounts.java
@@ -1,2 +1,34 @@
 public class Bank_Accounts {
+    private int BankNumber;
+    private int PIN;
+    private Bank bank;
+    public Bank_Accounts() {
+        BankNumber = this.BankNumber;
+        PIN = this.PIN;
+        bank = this.bank;
+    }
+
+    public int getBankNumber() {
+        return BankNumber;
+    }
+
+    public void setBankNumber(int bankNumber) {
+        BankNumber = bankNumber;
+    }
+
+    public int getPIN() {
+        return PIN;
+    }
+
+    public void setPIN(int PIN) {
+        this.PIN = PIN;
+    }
+
+    public Bank getBank() {
+        return bank;
+    }
+
+    public void setBank(Bank bank) {
+        this.bank = bank;
+    }
 }
