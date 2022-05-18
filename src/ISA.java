176aaabf6c56506cfc2402970b9e6a075e40bf13
yasum <yasum.abbas137@gmail.com>
2022-05-18 16:37:10 +0100

    Worked on ISA changes

---
+++ src/ISA.java
@@ -0,0 +1,30 @@
+public class ISA {
+    public static double Max_Annual_Deposit = 20_000;
+    public double InterestRate = 0.02;
+    public double balance;
+
+    public void setBalance(double balance){
+        this.balance = balance;
+    }
+
+    //The following methods need to be looked at and revised
+    public void transfer(double transferAmount){
+        this.balance = balance - transferAmount;
+    }
+
+    public void withdraw(double withDrawAmount){
+        this.balance = balance - withDrawAmount;
+    }
+
+    public void pay(double paymentAmount){
+        this.balance = balance - paymentAmount;
+    }
+
+    public void deposit(double depositAmount){
+        this.balance = balance - depositAmount;
+    }
+
+    public void addInterest(double interestAmount){
+        this.InterestRate += interestAmount;
+    }
+}
